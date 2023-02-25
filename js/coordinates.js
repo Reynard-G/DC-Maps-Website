f6c877bd974305b481eb321d93d9d8e972ecb041
Reynard-G <reynard.ean.gunawan@gmail.com>
2023-02-24 21:24:50 -0600

    better detailed map

--- js/coordinates.js
+++ js/coordinates.js
@@ -1,7 +1,7 @@
 // On mouse move, update the coordinates
 map.on('mousemove', function (e) {
-    const location = fromLatLngToLocation(e.latlng, 64, 1, 6);
-    document.getElementById("coordinates").innerHTML = `X: ${location.x} Y: ${location.y} Z: ${location.z}`;
+    const location = fromLatLngToLocation(e.latlng, 64, tilescale, mapzoomout);
+    document.getElementById("coordinates").innerHTML = `X: ${Math.round(location.x)} Y: ${Math.round(location.y)} Z: ${Math.round(location.z)}`;
 });
 
 // On mouse out, reset the coordinates
--- js/map.js
+++ js/map.js
@@ -1,9 +1,11 @@
 const worldtomap = [2.0, 0.0, -1.2246467991473532E-16, -1.2246467991473532E-16, 0.0, -2.0, 0.0, 1.0, 0.0];
 const maptoworld = [0.5, -3.061616997868383E-17, 0.0, 0.0, 0.0, 1.0, -3.061616997868383E-17, -0.5, 0.0];
+const tilescale = 1;
+const mapzoomout = 6;
 
 function fromLocationToLatLng(location, tilescale, mapzoomout) {
-    var lat = worldtomap[3] * location.x + worldtomap[4] * location.y + worldtomap[5] * location.z + 318,
-        lng = worldtomap[0] * location.x + worldtomap[1] * location.y + worldtomap[2] * location.z;
+    var lat = worldtomap[3] * location.x + worldtomap[4] * location.y + worldtomap[5] * location.z + 248,
+        lng = worldtomap[0] * location.x + worldtomap[1] * location.y + worldtomap[2] * location.z + 4;
 
     return new L.LatLng(
         -(((128 << tilescale) - lat) / (1 << mapzoomout))
@@ -15,14 +17,14 @@ function fromLocationToLatLng(location, tilescale, mapzoomout) {
 function fromLatLngToLocation(latlng, y, tilescale, mapzoomout) {
     var lat = (128 << tilescale) + latlng.lat * (1 << mapzoomout),
         lng = latlng.lng * (1 << mapzoomout),
-        x = maptoworld[0] * lng + maptoworld[1] * lat + maptoworld[2] * y,
-        z = maptoworld[6] * lng + maptoworld[7] * lat + maptoworld[8] * y + 159;
+        x = maptoworld[0] * lng + maptoworld[1] * lat + maptoworld[2] * y - 2,
+        z = maptoworld[6] * lng + maptoworld[7] * lat + maptoworld[8] * y + 124;
 
-    return { x: Math.round(x), y: Math.round(y), z: Math.round(z) };
+    return { x: x, y: y, z: z };
 }
 
-// Set the view when loaded in to be [-1, -60]
-const spawn = fromLocationToLatLng({ x: 2725, y: 64, z: 4153 }, 1, 6);
+// Set the view when loaded in to be spawn
+const spawn = fromLocationToLatLng({ x: 2725, y: 64, z: 4153 }, tilescale, mapzoomout);
 const map = L.map('map', {
     crs: L.CRS.Simple,
     zoom: 6,
@@ -31,12 +33,11 @@ const map = L.map('map', {
 
 
 // Create the maps along with the tile layer
-const bounds = fromLocationToLatLng({ x: 6000, y: 64, z: 6000 }, 1, 6);
+const bounds = fromLocationToLatLng({ x: 6000, y: 64, z: 6000 }, tilescale, mapzoomout);
 var redmontMap = L.tileLayer('../images/Leaflet Maps/Redmont/{z}/{x}/{y}.png', {
-    minZoom: 0,
-    maxZoom: 8,
+    minZoom: 2,
+    maxZoom: 7,
     bounds: [[0, 0], [bounds.lat, bounds.lng]],
-    maxNativeZoom: 6,
     noWrap: true,
     unloadInvisibleTiles: true,
     reuseTiles: true
@@ -50,7 +51,7 @@ async function getHeatmapData() {
     });
 
     return txt.map(position => {
-        const coords = fromLocationToLatLng({ x: position[0], y: 64, z: position[1] }, 1, 6);
+        const coords = fromLocationToLatLng({ x: position[0], y: 64, z: position[1] }, tilescale, mapzoomout);
         return [coords.lat, coords.lng, .01];
     });
 };
